a6484d117c65e01453fd46718cf7679810eba4ef
Trevor Liu <trevorliu@gmail.com>
2022-09-13 23:47:25 -0700

    started classes (all same file i am lazy)

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,71 +1,90 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './normalize.css';
+import './sakura.css';
 import './index.css';
 
 
-class Square extends React.Component {
+class Menu extends React.Component {
+    constructor(props){
+    super(props);
+
+    }
+    render() {
+      return (
+        <div className="Menu">
+            <button className = "Start">'Start'</button>
+        </div>
+      );
+    }
+  }
+
+  class Board extends React.Component {
+    constructor(props){
+    super(props);
+
+    }
+
+    generate(){
+
+
+
+    }
+
+    renderCard(i){
+        return(        
+            <Card value = {val}/>
+        )
+        }
+
+    render() {
+      return (
+        <div className="Board">
+            {this.renderCard(1)}
+            {this.renderCard(2)}
+            {this.renderCard(3)}
+            {this.renderCard(4)}
+        </div>
+      );
+    }
+  }
+
+
+  class Card extends React.Component {
     constructor(props){
     super(props);
     this.state = {
         value: null,
     };
 
+
     }
     render() {
       return (
-        <button className="square" onClick={()=> {this.setState({value: 'X'})}}>
-          {this.state.value}
-        </button>
-      );
-    }
-  }
-  
-  class Board extends React.Component {
-    renderSquare(i) {
-      return <Square value={i}/>;
-    }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+        <div className="Card">
+          {this.value}    
         </div>
       );
     }
   }
+
   
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
+class Game extends React.Component {
+render() {
+    return (
+    <div className="game">
+        <div className="game-board">
+        <Menu />
+        <Board />
         </div>
-      );
-    }
-  }
+        <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+        </div>
+    </div>
+    );
+}
+}
   
   // ========================================
   
