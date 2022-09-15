abb29f87ef492234eedff902d5525d9c856d3fd2
Trevor Liu <trevorliu@gmail.com>
2022-09-15 01:01:41 -0700

    cards actually can pass a state.value now

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -21,51 +21,70 @@ class Menu extends React.Component {
 
   class Board extends React.Component {
     constructor(props){
-    super(props);
+      super(props);
 
+      this.state = {
+        nums: Array(4).fill(null),
+        input: '',
+        guessResult: '',
+      }
+
+      this.generate()
     }
 
+    
     generate(){
-
-
-
+      const nums = this.state.nums.slice();
+      for (let i = 0; i < 4; i++){
+        this.state.nums[i] = Math.floor(Math.random()*8) + 1;
+        this.setState({nums: nums})
+      }
     }
 
-    renderCard(i){
-        return(        
-            <Card value = {val}/>
-        )
-        }
+    guess(){
 
+    }
+    
+    renderCard(i){
+      return(        
+        <div className="Card">
+          <h3>{this.state.nums[i]}</h3>    
+          <br></br>
+        </div>
+      )
+    }
+
+  inputUpdate(event){
+      const val = event.target.value;     
+      this.setState({
+        input: val
+      });
+    }
+
+    
+
+
+    
     render() {
       return (
-        <div className="Board">
+        <div>
+          <div className="Board">
+            {this.renderCard(0)}
             {this.renderCard(1)}
             {this.renderCard(2)}
             {this.renderCard(3)}
-            {this.renderCard(4)}
+          </div>
+
+          <div className="Board">
+            <input className = "Input" value = {this.state.input} onChange = {event => {this.inputUpdate(event)}}></input>
+            <button onClick = {this.guess()}>Submit</button>
+          </div>
+          <p>{this.state.guessResult}</p>
         </div>
       );
     }
-  }
 
 
-  class Card extends React.Component {
-    constructor(props){
-    super(props);
-    this.state = {
-        value: null,
-    };
-
-
-    }
-    render() {
-      return (
-        <div className="Card">
-          {this.value}    
-        </div>
-      );
-    }
   }
 
   
@@ -73,13 +92,9 @@ class Game extends React.Component {
 render() {
     return (
     <div className="game">
-        <div className="game-board">
-        <Menu />
-        <Board />
-        </div>
-        <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
+          <div className="game-board">
+            <Board 
+            />
         </div>
     </div>
     );
@@ -91,3 +106,40 @@ render() {
   const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(<Game />);
   
+  //Checks to see if you can +, /, -, * the elements inside the array in order to reach 24
+  var test24 = function(list){
+
+    if(list.length == 1 && list[0] == 24) return true; 
+
+    for(let i = 0; i < list.length; i++){
+      for(let j = 0; j < list.length; j++){
+        if(j != i) {
+
+          //I am not smart enough to do this nicely in 1 line each
+          let tList = list.slice();
+          tList[i] = tList[i] + tList[j];
+          tList.splice(j, 1);
+          if(test24(tList)) return true;
+
+          tList = list.slice();
+          tList[i] = tList[i] - tList[j];
+          tList.splice(j, 1);
+          if(test24(tList)) return true;
+
+          tList = list.slice();
+          tList[i] = tList[i] * tList[j];
+          tList.splice(j, 1);
+          if(test24(tList)) return true;
+
+          tList = list.slice();
+          tList[i] = tList[i] / tList[j];
+          tList.splice(j, 1);
+          if(test24(tList)) return true;
+
+        }
+      }
+    }
+    return false;
+  }
+
+
